9937eec6bfd8f7a85d621efa2bf214ec4e842088
Krico16 <bash_stone_u@hotmail.com>
2021-12-03 19:53:13 -0500

    Database configured

--- config/database.js
+++ config/database.js
@@ -1,21 +1,21 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
-  defaultConnection:'default',
-  connections:{
-    connector:'bookshelf',
-    settings:{
-      client:'postgres',
-      host:env('DATABASE_HOST','127.0.0.1'),
-      port:env.int('DATABASE_PORT','5432'),
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', '5432'),
       database: env('DATABASE_NAME', 'verus'),
       username: env('DATABASE_USERNAME', 'admin'),
-      password: env('DATABASE_PASSWORD', 'admin')
+      password: env('DATABASE_PASSWORD', 'admin'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
-    options: {
-      ssl: false
-    }
-  }
+    debug: false,
+  },
   /*
   connection: {
     client: 'sqlite',
